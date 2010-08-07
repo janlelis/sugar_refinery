43993597a377d6e4d179d1432f73cc236c6b088d
Jan Lelis <mail@janlelis.de>
2010-08-07 13:35:54 +0200

    fixed hash and added specs

--- lib/zucker/hash.rb
+++ lib/zucker/hash.rb
@@ -1,16 +1,16 @@
 class Hash
   def self.zip(keys,values)
-    Hash[ *[1,2,3].zip( [4,5,6] ).flatten ]
+    Hash[ *keys.zip( values ).flatten ]
   end
 
   def <<(other)
     case
     when other.is_a?(Hash)
       merge! other
-    when other.respond_to?(:to_splat)
+    when other.is_a?(Enumerable) || other.respond_to?(:to_splat)
       merge! Hash[*other]
     else
-      raise TypeError, 'can only append other Hashs and Enumerables'
+      raise TypeError, 'can only append other Hashs and Enumerables (or Classes that implement to_splat)'
     end
   end
 end
