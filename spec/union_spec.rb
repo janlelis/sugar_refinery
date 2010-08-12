f83e7a3eb1ad314b63d0e8b23b2c06fdd4daf3f9
Jan Lelis <mail@janlelis.de>
2010-08-12 22:43:42 +0200

    added regexp union spec

--- spec/union_spec.rb
+++ spec/union_spec.rb
@@ -2,6 +2,8 @@ require 'zucker/union'
 
 shared_examples_for "Regexp.union operator" do
   it "should create an Regexp.union of both operands" do
+    (/Ruby\d/ | /test/i | "cheat").should ==
+      Regexp.union( Regexp.union( /Ruby\d/, /test/i ), "cheat" )
   end
 end
 
