53939fbc6144892abc9e3b0f5adef36ddcb4e134
Jan Lelis <mail@janlelis.de>
2010-08-13 15:49:22 +0200

    added Object#not

---
+++ spec/not.rb
@@ -0,0 +1,8 @@
+require 'zucker/not'
+
+describe 'Object#not' do
+  it "should return an object on which all methods are redirected to it's receiver object, but return an inverted boolean" do
+    [1,2,3].not.empty?.should == true
+    nil.not.nil?.should == false
+  end
+end
