a945a5a9535f91f68364949166dd0ed92e6c23d8
Jan Lelis <mail@janlelis.de>
2010-09-07 01:24:27 +0200

    restored better egonil semantics (no more default value)

--- lib/zucker/egonil.rb
+++ lib/zucker/egonil.rb
@@ -1,12 +1,23 @@
 require 'zucker'
 
-def egonil(nil_value = nil)
+# code by Yohan, slightly edited and comments by me
+def egonil(&block)
+  # grip methods
+  ori_method_missing   = NilClass.instance_method(:method_missing)
+  catch_method_missing = NilClass.instance_method(:catch_method_missing)
+  # activate ego mode
+  NilClass.send :define_method, :method_missing, catch_method_missing
+  # run code
   yield
-rescue NoMethodError => e
-  if e.message =~ /NilClass$/
-    nil_value
-  else
-    raise NoMethodError
+ensure
+  # no matter what happens: restore default nil behaviour
+  NilClass.send :define_method, :method_missing, ori_method_missing
+end
+
+# this is the ego nil
+class NilClass
+  def catch_method_missing(m, *args, &block)
+    nil
   end
 end
 
