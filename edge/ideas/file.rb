1dc886640b08aa80131b6c0d52b42c261b81f398
Jan Lelis <mail@janlelis.de>
2011-01-16 17:42:38 +0100

    [edge-ideas] File.gsub filename, regex_hash

---
+++ edge/ideas/file.rb
@@ -0,0 +1,17 @@
+class << File
+  def gsub(filename, regex_hash)
+    data = File.read filename
+    File.open(filename,'w'){ |file|
+      regex_hash.each{ |regex, new_string|
+        regex = regex.to_s unless regex.is_a? Regexp
+
+        if new_string.is_a? Proc
+          data.gsub! regex, &new_string
+        else
+          data.gsub! regex, new_string
+        end
+      }
+      file.print data
+    }
+  end
+end
