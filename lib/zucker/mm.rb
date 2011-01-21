303fd9930a15030d7c9869f7ea02e492752c4d24
Jan Lelis <mail@janlelis.de>
2011-01-21 15:36:57 +0100

    mm tweaks

--- lib/zucker/mm.rb
+++ lib/zucker/mm.rb
@@ -2,28 +2,34 @@ require 'zucker'
 
 module Kernel
   def m(levels = 1)
+    # output helper
+    mputs = proc{ |e| puts e.map(&:to_s)*'  ' }
+
+    # how to get results
     if self.is_a? Module
       klass, method_function = self, :public_methods
     else
       klass, method_function = self.class, :public_instance_methods
-
-      eigen = self.singleton_methods
-      if !eigen.empty?
-        puts :Eigenclass # sorry for not being up to date, I just love the word
-        p self.singleton_methods
-      end
     end
 
+    # eigenclass
+    eigen = self.singleton_methods
+    if !eigen.empty?
+      puts '= Eigenclass' # sorry for not being up to date, I just love the word
+      mputs[ self.singleton_methods ]
+    end
+
+    # display
     levels.times{ |level|
       if cur = klass.ancestors[level]
-        p cur                               # put class name
-        p cur.send method_function, false   # put methods of the class
+        puts "= #{cur}"  # put class name
+        mputs[ cur.send method_function, false ] # put methods of the class
       else
         break
       end
     }
 
-    self # or whatever
+    nil
   end
 
   alias mm          m
