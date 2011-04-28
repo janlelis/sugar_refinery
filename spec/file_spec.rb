327f5de7cc1993cd01e97cddc8b3f65b4440ce42
Jan Lelis <mail@janlelis.de>
2011-04-29 00:13:33 +0200

    file cube doc/spec improvements

--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -1,12 +1,28 @@
 require 'zucker/file'
 require 'fileutils'
 
-describe 'File#gsub' do
-  it 'should read filename in arg1, substitute every key in the arg2 with its value and save the file' 
+describe 'File.gsub' do
+  let :random_filename do
+    'test_' + (0..20).map{|e| [*'a'..'z'].send RUBY_VERSION > '1.9' ? :sample : :choice }*''
+  end
+
+  it 'should read filename in arg1, substitute every key in the arg2 with its value and save the file' do
+    File.open(random_filename,'w'){ |file|
+      file.print 'should read filename in arg1, substitute every key in the arg2 with its value and save the file'
+    }
+    File.gsub random_filename,
+      /read/ => 'write',
+      /\d+/  => proc{|m| (m.to_i+1).to_s }
+
+    File.read(random_filename).should ==
+      'should write filename in arg2, substitute every key in the arg3 with its value and save the file' 
+
+    FileUtils.rm random_filename
+  end
 end
 
-describe 'File#delete!' do
-  let :random_filename   do
+describe 'File.delete!' do
+  let :random_filename do
     'test_' + (0..20).map{|e| [*'a'..'z'].send RUBY_VERSION > '1.9' ? :sample : :choice }*''
   end
 
