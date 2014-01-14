cb18fbbad96d0b383d33e493a72fe81d0d0702be
Jan Lelis <mail@janlelis.de>
2014-01-14 17:47:58 +0100

    let's go, use first refinements!

--- lib/zucker/case.rb
+++ lib/zucker/case.rb
@@ -1,14 +1,15 @@
 require 'zucker'
-module Zucker Case = true end
 
-class String
-  def to_snake
-    gsub(/(?<!^)[A-Z]/) do "_#$&" end.downcase
-  end
-  
-  def to_camel
-    gsub(/(?:^|_)([a-z])/) do $1.upcase end
+module Zucker
+  module Case
+    refine String do
+      def to_snake
+        gsub(/(?<!^)[A-Z]/) do "_#$&" end.downcase
+      end
+
+      def to_camel
+        gsub(/(?:^|_)([a-z])/) do $1.upcase end
+      end
+    end
   end
 end
-
-# J-_-L
