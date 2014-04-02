a2cb6ae6ce47489cec96b2875c7e4f1683a947da
Michael Parks <tk@tkware.info>
2014-04-02 08:10:09 -0600

    Add "to_range" methods
    This is a bit of code I find myself using a great deal in command line applications, and your library is extensive enough that someone else may find it helpful. Hope this helps!

--- lib/zucker/string_extras.rb
+++ lib/zucker/string_extras.rb
@@ -10,6 +10,24 @@ module Zucker
       def lchomp!(arg = $/)
         replace reverse.chomp(arg).reverse
       end
+      
+      def to_r
+        a = self.partition(/\(.+?\)/)
+        fail "No range in string" if a[0].empty?
+        fail "Invalid range #{a}" if a.select { |v| v.match(/[A-Za-z]/) }
+        eval a[1]
+      end
+ 
+      def to_r_leading_zero
+        a = self.partition(/\(.+?\)/)
+        fail "No range in string" if a[0].empty?
+        fail "Invalid range #{a}" if a.select { |v| v.match(/[A-Za-z]/) }
+        r = eval a[1]
+        eval "\'#{"0" + r.first.to_s}\'..\'#{r.last.to_s}\'" if r.first.to_s.size == 1
+      end
+      
+      alias_method :to_range, :to_r
+      alias_method :to_range_leading_zero, :to_r
     end
   end
 end
