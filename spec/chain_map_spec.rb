9b4bdebcfe2580661f5fa3c2988a268ab99db992
Evan Senter <evansenter@gmail.com>
2014-04-14 12:57:44 -0400

    ChainMap

---
+++ spec/chain_map_spec.rb
@@ -0,0 +1,16 @@
+require "zucker/chain_map"
+using Zucker::ChainMap
+
+describe "Array#chain_map" do
+  it "should repeatedly chain symbols as map calls" do
+    list = [-3, -2, -1]
+
+    list.chain_map(:abs, :to_s) == %w{3 2 1}
+  end
+
+  it "should repeatedly chain blocks as map calls" do
+    list = [-3, -2, -1]
+
+    list.chain_map(->(e) { e ** 2 }, ->(e) { e * -1 }) == [-9, -4, -1]
+  end
+end
